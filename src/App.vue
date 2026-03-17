f3f2c46c490b599e915f9cd8dd817342185d006d
Albert0 <rosalesalberto217@gmail.com>
2026-03-17 08:57:50 -0600

    Arreglos en el archivo App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,31 @@
 <script setup>
-
 import { useCarritoStore } from './stores/useCarritoStore'
 
 const carritoStore = useCarritoStore()
-
 </script>
 
 <template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+    <div class="container">
+      <a class="navbar-brand fw-bold"> Tienda Jaguares </a>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+      <div>
+        <router-link class="btn btn-outline-light me-2" to="/"> Inicio </router-link>
 
-<div class="container">
+        <router-link class="btn btn-outline-light me-2" to="/productos"> Productos </router-link>
 
-<a class="navbar-brand fw-bold">
-🐆 Tienda Jaguares
-</a>
+        <router-link class="btn btn-outline-light me-2" to="/productos/crear">
+          Crear Producto
+        </router-link>
 
-<div>
+        <router-link class="btn btn-warning" to="/carrito">
+          🛒 Carrito ({{ carritoStore.totalArticulos }})
+        </router-link>
+      </div>
+    </div>
+  </nav>
 
-<router-link class="btn btn-outline-light me-2" to="/">
-Inicio
-</router-link>
-
-<router-link class="btn btn-outline-light me-2" to="/productos">
-Productos
-</router-link>
-
-<router-link class="btn btn-outline-light me-2" to="/productos/crear">
-Crear Producto
-</router-link>
-
-<router-link class="btn btn-warning" to="/carrito">
-🛒 Carrito ({{ carritoStore.totalArticulos }})
-</router-link>
-
-</div>
-
-</div>
-
-</nav>
-
-<div class="container mt-4">
-
-<router-view/>
-
-</div>
-
+  <div class="container mt-4">
+    <router-view />
+  </div>
 </template>
